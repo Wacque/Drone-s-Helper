353904e8f27a39b558081e2ca1d2938896f6ec92
agui <rengui.wu@advancegroup.com>
2023-08-18 00:19:01 +0800

    pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o18us2gs2"
+    "revision": "0.lr5bfn2o4m8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,29 @@ export default defineConfig({
         react(),
         VitePWA({
             registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
             workbox: {
                 clientsClaim: true,
                 skipWaiting: true
+            },
+            includeAssets: ['u16.svg', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+            manifest: {
+                name: 'GoodWorkingDays',
+                theme_color: '#000000',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
             }
         })
     ],
@@ -22,6 +42,6 @@ export default defineConfig({
         }
     },
     build: {
-        outDir: "/Users/wurengui/Code/code-self/vit-base-patch16-224-in21k_human_activity_recognition/dist",
+        // outDir: "/Users/wurengui/Code/code-self/vit-base-patch16-224-in21k_human_activity_recognition/dist",
     }
 })
